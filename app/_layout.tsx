3ee07bbfb3226b21d183fdcfaf11aa1046987cf9
YasinMK <90131469+maxxkh@users.noreply.github.com>
2025-08-07 01:13:31 -0700

    Remove duplicate useFrameworkReady import

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,5 @@
 import Stack from 'expo-router/stack';
 import { useFrameworkReady } from '../hooks/useFrameworkReady'
-import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 export default function Layout() {
   useFrameworkReady();
