b26c3be0351df7995b752603ba7c607f25a5c298
YasinMK <90131469+maxxkh@users.noreply.github.com>
2025-08-07 01:12:22 -0700

    Added useFrameworkReady.ts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,10 @@
 import Stack from 'expo-router/stack';
-import { StyleSheet } from 'nativewind';
+import { StyleSheet } from 'nativewind'
+import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 StyleSheet.setFlag('darkMode', 'class');
 
 export default function Layout() {
+  useFrameworkReady();
   return <Stack screenOptions={{ headerShown: false }} />;
 }
