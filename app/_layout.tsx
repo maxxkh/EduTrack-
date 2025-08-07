89f80649c412a9770acd5026b0d577add6824a5a
YasinMK <90131469+maxxkh@users.noreply.github.com>
2025-08-07 01:12:20 -0700

    Fix dark mode configuration error

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,7 @@
 import Stack from 'expo-router/stack';
+import { StyleSheet } from 'nativewind';
 
+StyleSheet.setFlag('darkMode', 'class');
 
 export default function Layout() {
   return <Stack screenOptions={{ headerShown: false }} />;
