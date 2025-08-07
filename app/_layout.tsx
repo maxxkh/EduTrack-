32e8d4e9f9ecdfaecc4b0fc1760bcaa895225779
YasinMK <90131469+maxxkh@users.noreply.github.com>
2025-08-07 01:13:33 -0700

    Updated _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,6 @@
 import Stack from 'expo-router/stack';
 import { useFrameworkReady } from '../hooks/useFrameworkReady'
+import { useFrameworkReady } from '@/hooks/useFrameworkReady'
 
 export default function Layout() {
   useFrameworkReady();
